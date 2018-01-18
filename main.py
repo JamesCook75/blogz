41221ba0f097fb37601e6fd25336f5b9d920ddf9
James Cook <tbbucs10@gmail.com>
2018-01-18 18:50:10 -0500

    Incorporated MVC structure

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy, Pagination
-
-import datetime 
-
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
-app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
-app.secret_key = 'iewgbaklsjhd'
-
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(300))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    date_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-
-    def __init__(self, title, body, owner):
-        self.title = title
-        self.body = body
-        self.owner = owner
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(120))
-    blogs = db.relationship('Blog', backref='owner')
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+from models import User, Blog
+from app import db, app
 
 @app.before_request        
 def require_login():
@@ -40,7 +11,7 @@ def require_login():
 
 @app.route("/", methods=['GET'])
 def home():
-    return redirect('/blog')
+    return redirect('/blog/1')
 
 @app.route("/login", methods=['POST', 'GET'])
 def login():
@@ -99,7 +70,6 @@ def index():
     users = User.query.order_by(User.username).all()
     return render_template('index.html', users=users)
 
-
 @app.route('/blog/<int:page>', methods=['GET'])
 def blog(page):
     per_page = 5
@@ -109,7 +79,7 @@ def blog(page):
         blog = Blog.query.filter_by(id=blog_id).first()
         return render_template('blog_item.html', blog=blog)
     if author_id:
-        blogs = Blog.query.filter_by(owner_id=author_id).all()
+        blogs = Blog.query.filter_by(owner_id=author_id).order_by(Blog.date_time.desc()).all()
         return render_template('single_user.html', blogs=blogs)
 
     blogs = Blog.query.order_by(Blog.date_time.desc()).paginate(per_page=per_page, page=page, error_out=True)
@@ -138,7 +108,6 @@ def newpost():
         return render_template('blog_item.html', blog=new_blog)
     
     return render_template('newpost.html')
-
 
 if __name__ == '__main__':
     app.run()
---
+++ models.py
@@ -0,0 +1,24 @@
+from app import db
+import datetime 
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(300))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_time = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, title, body, owner):
+        self.title = title
+        self.body = body
+        self.owner = owner
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
